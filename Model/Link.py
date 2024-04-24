905dd1e9adbca3a5830d64f8a11fdee5f26a9c5f
TheBonnec <thomaslebonnec@icloud.com>
2024-04-25 01:17:30 +0200

    Premier JSON

--- Model/Link.py
+++ Model/Link.py
@@ -1,3 +1,10 @@
+from Supplier import Supplier
+from Customer import Customer
+
+
 class Link:
-    def __init__(self):
-        pass
+    def __init__(self, supplier: Supplier, customer: Customer, cost: int, units: int = 0):
+        self.supplier = supplier
+        self.customer = customer
+        self.cost = cost
+        self.units = units
