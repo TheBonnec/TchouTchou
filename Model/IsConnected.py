8554dc0aa45e91a1383ba005c207064cbc8ce96e
Izura7 <94544223+Izura7@users.noreply.github.com>
2024-05-02 13:30:22 +0200

    fin fonction isConnected, J'attends le verifyCycle

--- Model/IsConnected.py
+++ Model/IsConnected.py
@@ -15,16 +15,26 @@ def isConnected(TP : TransportationProblem):
     currentVertex = TP.suppliers[0]
     queue = []
     queue.append(currentVertex)
+    for i in listLinkExisting:
+        if (i.supplier == currentVertex):
+            listNextVertices.append(i.customer)
+        elif(i.customer == currentVertex):
+            listNextVertices.append(i.supplier)
+
     while listNextVertices != []:
+        listNextVertices = []
+        for i in listLinkExisting:
+            if (i.supplier == currentVertex and i.customer not in visitedVertices):
+                listNextVertices.append(i.customer)
+            elif(i.customer == currentVertex and i.supplier not in visitedVertices):
+                listNextVertices.append(i.supplier)
         visitedVertices.append(queue[-1])
         queue.pop()
-        for i in listLinkExisting:
-            if (i.supplier == currentVertex):
-                listNextVertices.append(i.customer)
-            elif(i.customer == currentVertex):
-                listNextVertices.append(i.supplier)
-        queue.append(p for p in listNextVertices if p not in visitedVertices)
-        currentVertex = queue[-1]
+        for i in listNextVertices:
+            if i not in visitedVertices:
+                queue.append(i)
+        if queue != []:
+            currentVertex = queue[-1]
     if (len(visitedVertices) == (len(listVertices))):
         return TP
     else:
--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from Controller.SteppingStoneViewController import SteppingStoneViewController
 from Model.TransportationProblem import TransportationProblem
 from Model.IsConnected import isConnected
+from Controller.TransportationProblemViewController import TransportationProblemViewController
 
-<<<<<<< HEAD
 # Create the problem instance from a JSON file
-tp = TransportationProblem("TextFiles/TransportationProblem5.json")
-controller = SteppingStoneViewController(tp)
-constraintValue = controller.calculateConstraint()
-print("Transportation Constraint:", constraintValue)
-=======
-class MyView():
-    TP = TransportationProblem("TextFiles/TransportationProblem3.json")
-
-    #def __init__(self):
-     #super().__init__()
-
-
-MyView()
->>>>>>> 2470b88 (test isConnected et debut ToNonDegenerate)
+tp = TransportationProblem("TextFiles/TransportationProblem8.json")
+tp = TransportationProblemViewController.NordWestAlgorithm(tp)
+for i in tp.links:
+    i.print()
+print(isConnected(tp))
