8b832122cfa4330d055f1efc70063af64773383b
charline blanc <charline.blanc@efrei.net>
2024-05-03 15:36:26 +0200

    calculatePotentials

--- Controller/SteppingStoneViewController.py
+++ Controller/SteppingStoneViewController.py
@@ -1,11 +1,31 @@
-class SteppingStoneViewController:
-    def __init__(self, transportationProblem):
-        self.tp = transportationProblem
+from Model.TransportationProblem import TransportationProblem
+from Model.Supplier import Supplier
+from Model.Customer import Customer
 
+class CostLink:
+    def __init__(self, supplier: Supplier, customer: Customer, cost: int):
+        self.supplier = supplier
+        self.customer = customer
+        self.cost = cost
+
+class SteppingStoneViewController:
+    def __init__(self):
+        pass  
+    
+    def calculatePotentials(self, tp: TransportationProblem):
+        costLinks = []
+        for supplier in tp.suppliers:
+            for customer in tp.customers:
+                cost = supplier.provision - customer.order
+                costLinks.append(CostLink(supplier, customer, cost))
+        return costLinks
+
+        
     def calculateConstraint(self):
         totalProvisions = sum(supplier.provision for supplier in self.tp.suppliers)
         totalOrders = sum(customer.order for customer in self.tp.customers)
-        totalShipped = sum(link.units for link in self.tp.links)  # Make sure 'units' is the name of the attribute in Link class
-
+        totalShipped = sum(link.units for link in self.tp.links) 
+        
         print(f"Total provisions: {totalProvisions}, Total orders: {totalOrders}, Total shipped: {totalShipped}")
         return min(totalProvisions, totalOrders, totalShipped)
+    
