4607c40013daebc71f4a80fc0871d6a475c19460
Izura7 <94544223+Izura7@users.noreply.github.com>
2024-05-04 19:19:33 +0200

    Fini ToNonDegenerate and isConnected

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from Controller.SteppingStoneViewController import SteppingStoneViewController
 from Model.TransportationProblem import TransportationProblem
-from Controller.TransportationProblemViewController import TransportationProblemViewController
-from View.View import View
-
-View().afficher_proposition_transport(tp, solution)
-
-from Controller.verifyCycle import isAcylique
-print(isAcylique(solution))
-
-from Model.IsConnected import isConnected
-from Controller.TransportationProblemViewController import TransportationProblemViewController
 
 
 class MyView():
